9edd55807c3d1d3f461b79df0ceadd3232d7e965
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-04 17:04:11 -0300

    leitura de arquivo externo

---
+++ Python/Codigos/leitura.py
@@ -0,0 +1,15 @@
+# with open(file="Python_M2_support material02.ipynb", mode='r', encoding='utf8') as arquivo:
+#     conteudo = arquivo.read();
+# print(conteudo)
+
+conteudo = []
+
+with open(file="Python_M2_support material02.ipynb", mode='r', encoding='utf8') as arquivo:
+    linha = arquivo.readline()
+    while linha:
+        conteudo.append(linha)
+        linha = arquivo.readline()
+
+#print(conteudo)
+for linha in conteudo:
+    print(linha)
