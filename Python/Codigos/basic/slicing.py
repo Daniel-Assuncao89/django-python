0e06e6c16773a6b877e93de8d9ebd105a970625f
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-10 08:47:51 -0300

    Sciling: anotações

--- Python/Codigos/basic/slicing.py
+++ Python/Codigos/basic/slicing.py
@@ -11,3 +11,9 @@ print(string[-1::])
 #deixar vazio significa q irá até o final do array
 print(numeros[0::])
 print(string[0::])
+
+#Slicing do começo até a posicao 5
+print(numeros[:5])
+
+#Slicing até o final
+print(numeros[2:])
