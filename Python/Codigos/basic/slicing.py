50e497b1479b87e99295c916744b911657aa58d2
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-10 08:35:24 -0300

    AnSlicing: anotações

---
+++ Python/Codigos/basic/slicing.py
@@ -0,0 +1,13 @@
+numeros = [1, 2, 3, 4, 5]
+string = "hello world"
+
+#indice inicial, indice final e sequencia
+print(numeros[ : : ])
+
+#mostrar apenas o ultimo caracter
+print(numeros[-1::])
+print(string[-1::])
+
+#deixar vazio significa q irá até o final do array
+print(numeros[0::])
+print(string[0::])
