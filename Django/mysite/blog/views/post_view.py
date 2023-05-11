74d490f286e50294b802ec41746e95b3f4264793
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-11 15:18:54 -0300

    Django template, html e css

--- Django/mysite/blog/views/post_view.py
+++ Django/mysite/blog/views/post_view.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
 from django.views import generic
 
+from blog.models import Post
 
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello World')
+
+# toda vez q bater na url home, vai executar a queryset e renderizar no index.html
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
