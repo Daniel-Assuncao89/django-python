ffb515547fa7f631e0b6b3a6d547c9910d60d28a
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-12 12:57:39 -0300

    Versão 3 do desafio

--- Python/Codigos/desafiosCesar/inteiroParaString.py
+++ Python/Codigos/desafiosCesar/inteiroParaString.py
@@ -13,39 +13,107 @@ def menu(opcao: int, controle: bool) -> bool:
         return controle
 
 
+def unidade(numerosList: list, indice):
+
+        if int(numerosList[indice]) == 0:
+            numerosList[indice] = "zero"
+            return numerosList
+        elif int(numerosList[indice]) == 1:
+            numerosList[indice] = "um"
+            return numerosList
+        elif int(numerosList[indice]) == 2:
+            numerosList[indice] = "dois"
+            return numerosList
+        elif int(numerosList[indice]) == 3:
+            numerosList[indice] = "tres"
+            return numerosList
+        elif int(numerosList[indice]) == 4:
+            numerosList[indice] = "quatro"
+            return numerosList
+        elif int(numerosList[indice]) == 5:
+            numerosList[indice] = "cinco"
+            return numerosList
+        elif int(numerosList[indice]) == 6:
+            numerosList[indice] = "seis"
+            return numerosList
+        elif int(numerosList[indice]) == 7:
+            numerosList[indice] = "sete"
+            return numerosList
+        elif int(numerosList[indice]) == 8:
+            numerosList[indice] = "oito"
+            return numerosList
+        elif int(numerosList[indice]) == 9:
+            numerosList[indice] = "nove"
+            return numerosList
+        else:
+            pass
+
+def dezena(numerosList: list, indice):
+
+        if int(numerosList[indice]) == 1:
+            numerosList[indice] = "dez"
+            return numerosList
+        elif int(numerosList[indice]) == 2:
+            numerosList[indice] = "vinte e "
+            return numerosList
+        elif int(numerosList[indice]) == 3:
+            numerosList[indice] = "trinta e "
+        elif int(numerosList[indice]) == 4:
+            numerosList[indice] = "quarenta e"
+            return numerosList
+        elif int(numerosList[indice]) == 5:
+            numerosList[indice] = "cinquenta e "
+            return numerosList
+        elif int(numerosList[indice]) == 6:
+            numerosList[indice] = "sessenta e "
+            return numerosList
+        elif int(numerosList[indice]) == 7:
+            numerosList[indice] = "setenta e "
+            return numerosList
+        elif int(numerosList[indice]) == 8:
+            numerosList[indice] = "oitenta e "
+            return numerosList
+        elif int(numerosList[indice]) == 9:
+            numerosList[indice] = "noventa e "
+            return numerosList
+        else:
+            pass
+
+
+def centena(numerosList: list, indice):
+    if int(numerosList[indice]) == 1:
+        numerosList[indice] = "cento e "
+        return numerosList
+    elif int(numerosList[indice]) == 2:
+        numerosList[indice] = "duzentos"
+        return numerosList
+    else:
+        pass
+
+
 def numeroParaInt(numero: int):
     if (numero <= 200 and numero >= 0):
         numerosList = str(numero)  # TypeCasting para str
         # print(numerosList)
         numerosList = list(numerosList)  # TypeCasting para list
         # print(numerosList)
+        # print(len(numerosList))
 
-        indice = 0
-        for num in numerosList:
-            print(num)
-            if (int(num) == 0):
-                numerosList[indice] = "zero"
-            elif (int(num) == 1):
-                numerosList[indice] = "um"
-            elif (int(num) == 2):
-                numerosList[indice] = "dois"
-            elif (int(num) == 3):
-                numerosList[indice] = "tres"
-            elif (int(num) == 4):
-                numerosList[indice] = "quatro"
-            elif (int(num) == 5):
-                numerosList[indice] = "cinco"
-            elif (int(num) == 6):
-                numerosList[indice] = "seis"
-            elif (int(num) == 7):
-                numerosList[indice] = "sete"
-            elif (int(num) == 8):
-                numerosList[indice] = "oito"
-            elif (int(num) == 9):
-                numerosList[indice] = "nove"
-            indice += 1
+        if len(numerosList) == 1:
+            resultado = unidade(numerosList, indice=0)
+            print(resultado)
 
-        print(numerosList)
+        elif len(numerosList) == 2:
+            resultado = dezena(numerosList, indice=0)
+            resultado += unidade(numerosList, indice=1)
+            print(resultado[0:2])
+
+        elif len(numerosList) == 3:
+            resultado = centena(numerosList, indice=0)
+            resultado += dezena(numerosList, indice=1)
+            resultado += unidade(numerosList, indice=2)
+            print(resultado[0:3])
+
     else:
         print("Numero maior que 200 ou negativo, voltando para o menu")
         return None
@@ -55,8 +123,8 @@ controle: bool = True
 # print(int('3') == 3) #Teste de comparacao p/ validar o retorno
 while controle:
     print("""
-    1- para sair da execucao
-    2- para entrar no programa
+    1- para entrar no programa
+    2- para sair da execucao
     """)
     opcao = int(input())
     print(opcao)
