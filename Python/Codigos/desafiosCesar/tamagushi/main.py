09fa2efc47fdb146c648cf0706785f0c40c2a9ed
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-23 10:49:34 -0300

    ajuste requirements. Tamagushi

---
+++ Python/Codigos/desafiosCesar/tamagushi/main.py
@@ -0,0 +1,35 @@
+from tamagushi import Tamagushi
+
+print("*" * 20)
+print("* ", "Bicinho Virtual", "*")
+print("*" * 20)
+nome = input("Qual o nome que você deseja para seu novo pet?")
+
+t1 = Tamagushi(nome=nome, fome=50, idade=0, saude=50)
+t1.humor(t1.fome, t1.saude)
+
+menu = int(input("""
+Digite 1 para alimentar
+Digite 2 para verificar o humor do pet
+Digite 3 para verificar a saude
+Digite 4 para Verificar a fome
+Digite 5 para verificar a idade
+"""))
+while 0 < menu <= 5:
+    if menu == 1:
+        t1.alimentar()
+    elif menu == 2:
+        t1.humor(t1.fome, t1.saude)
+    elif menu == 3:
+        print(t1.saude)
+    elif menu == 4:
+        print(t1.fome)
+    elif menu == 5:
+        print(t1.idade)
+    menu = int(input("""
+Digite 1 para alimentar
+Digite 2 para verificar o humor do pet
+Digite 3 para verificar a saude
+Digite 4 para Verificar a fome
+Digite 5 para verificar a idade
+"""))
---
+++ Python/Codigos/desafiosCesar/tamagushi/tamagushi.py
@@ -0,0 +1,32 @@
+""""
+Classe Bichinho Virtual:Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
+
+Atributos: Nome, Fome, Saúde e Idade b. Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade Obs: Existe mais uma informação que devemos levar em consideração, o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde, ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação por que ela pode ser calculada a qualquer momento.
+"""
+
+
+class Tamagushi:
+
+    def __init__(self, nome: str, fome: int, saude: int, idade: int):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def humor(self, fome: int, saude: int):
+        if fome >= 50 and saude >= 50:
+            nome = self.nome
+            print(f"{nome} está de bom humor")
+        elif fome < 50 and saude < 50:
+            nome = self.nome
+            print(f"{nome} está de mau humor")
+
+    def alimentar(self):
+        self.fome += 10
+        print(f"{self.nome} foi alimentado, agora sua fome é de: {self.fome}")
+        if self.fome > 80:
+            self.idade += 1
+            self.fome = 50
+            print(f"{self.nome} acaba de ficar 1 ano mais velho(a)")
+
+
