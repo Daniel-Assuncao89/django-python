a500b56315a9617a0fc34aaecc4e92ba6855446b
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-23 19:22:36 -0300

    cs50 23/05

--- Python/Codigos/cs50/students.py
+++ Python/Codigos/cs50/students.py
@@ -1,9 +1,12 @@
+import csv
+
+
 # with open("students.csv") as file:
 #     for line in file:
 #         name, house = line.rstrip().split(",")  # Atribui de forma respectiva, o valor antes da virgula a variavel name e o valor depois da virgula a variavel house
 #         print(f"{name} is in {house}")
 #
-students = []
+# students = []
 #
 # with open("students.csv") as file:
 #     for line in file:
@@ -12,12 +15,19 @@ students = []
 #
 # for student in sorted(students):
 #     print(student)
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
+#
+# for student in students:
+#     print(f"{student['name']} is in {student['house']}")
+#     print(student)
 
-for student in students:
-    print(f"{student['name']} is in {student['house']}")
-    print(student)
+name = input("what's your name? ")
+house = input("Where's your house? ")
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "house"])
+    writer.writerow({"name": name, "house": house})
