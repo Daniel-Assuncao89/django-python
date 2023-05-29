3b9c18847c4cc51353a40fe7486a144fd3c49770
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-05-29 12:24:53 -0300

    Dicionario

--- Python/Codigos/cs50/oop/student.py
+++ Python/Codigos/cs50/oop/student.py
@@ -1,13 +1,22 @@
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    # if student[0] == "Padma":
+    #     student[1] = "Ravenclaw"
+    # print(f"{student[0]} from {student[1]}")
+    if student["name"] == "padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
 
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    #return name, house  # apesar de passar 2 variaveis elas são retornadas como uma Tuple
-    return [name, house]  # Dessa forma retornamos uma list.
+    # name = input("Name: ")
+    # house = input("House: ")
+    # #return name, house  # apesar de passar 2 variaveis elas são retornadas como uma Tuple
+    # return [name, house]  # Dessa forma retornamos uma list.
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
 
 
 if __name__ == "__main__":
