d582df01acb3732eb721962da73d978daff9626c
Daniel Assuncao <assuncao.daniel89@gmail.com>
2023-05-30 14:22:57 -0300

    Venv: empacotamento de versão e libs

---
+++ testes-de-versao/projeto-com-python311/main.py
@@ -0,0 +1,7 @@
+# testando versionamento com match que só é suportado a partir da versão 3.10
+
+test = 1
+
+match test:
+    case 1:
+        print("Hello")
---
+++ testes-de-versao/projeto-com-python39/main.py
@@ -0,0 +1,4 @@
+# testando versionamento com match que só é suportado a partir da versão 3.10
+test = True
+if test:
+    print("hello")
