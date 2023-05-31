ac59a614fa06140028c6cbc052d26fd1aa9af588
Daniel Assuncao <assuncao.daniel89@gmail.com>
2023-05-31 09:32:31 -0300

    Indexação e ordenamento

--- Django/book-examples/example-1/mysite/blog/models.py
+++ Django/book-examples/example-1/mysite/blog/models.py
@@ -13,6 +13,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:  # Metadata
+        ordering = ['-publish']  # Define a default order (newest to oldest)
+        indexes = [
+            # Improve performance for queries filtering or ordering results by this field
+            models.Index(fields=['-publish']),
+        ]
+
     def __str__(self):
         return self.title
 
