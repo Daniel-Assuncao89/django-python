b7b0f2e20a4ccbb1e35ec35da5b367ab4489603f
Daniel Assuncao <assuncao.daniel89@gmail.com>
2023-06-01 19:22:21 -0300

    Refactor: normRecebe frase, sanitiza e normaliza a frase

--- Python/Codigos/desafiosCesar/palindromo.py
+++ Python/Codigos/desafiosCesar/palindromo.py
@@ -1,37 +1,22 @@
-"""
-Primeira solucao foi usar o metodo reverse() e comparar a palavra normal com a inversa, primeira dificuldade foi que o metodo reverse so se usa em estrutura list, então fui atras de como transformar em lista, depois a comparação não funcionava pq a variavel q deveria armazenar a palavra inversa retornava None e foi ai que descobri que o metodo reverse ele não retorna valores, ele so altera o original. Depois disso pensei em soluções mais verbosas, de armazenar o char de cada indice para fazer a comparação, um iniciando no indice 0 e o outro no final da palavra e ai comparando se as extremidades eram iguais. Mas achei q deveria ter uma solucao mais simples, entao comecei a ver minhas anotações sobre manipulação de array e foi ai que consegui resolver o problema. Decidi deixar alguns passos q fiz no codigo pra vcs terem essa visualização do caminho percorrido.
-"""
-palavra = input()
-# print(palavra[::-1])
-# print(palavra[0:] == palavra[::-1])
+import re
+from unidecode import unidecode
 
-def palindromo(palavra):
-    if ( palavra[0:] == palavra[::-1]):
-         print(f"{palavra} é um Palíndromo")
+
+def palindrome(word_sanitize, word):
+    if word_sanitize[0:] == word_sanitize[::-1]:
+        print(f"{word} é um Palíndromo")
     else:
-         print(f"A palavra : {palavra}")
-
-palindromo(palavra)
-
-"""
-def converterStrParaList(palavra: str) -> list:
-    converterPalavra = []
-    converterPalavra[0:] = palavra
-    return converterPalavra
+        print(f"A palavra : {word} não é um Palíndromo")
 
 
-def reverterLista(lista):
+def main():
+    word = input().lower().strip()
+    word_normalize = unidecode(word)
+    # print(word_normalize)
+    word_sanitize = re.sub(r"\s*\d*", "", word_normalize)
+    # print(word_sanitize)
+    palindrome(word_sanitize, word)
 
-     listaReversa = []
-     listaReversa = lista.reverse()
-     print(listaReversa)
-     return listaReversa
 
-listaPalavra = converterStrParaList(palavra)
-listaReversa = listaPalavra.reverse()
-print(listaReversa) # retorna none pq reverse() não retorna valores para armazenamento
-print(str(listaPalavra)) # tentativa de converter a lista de volta para string.
-palindromo(listaPalavra)
-
-reverterLista(listaPalavra)
-"""
+if __name__ == "__main__":
+    main()
