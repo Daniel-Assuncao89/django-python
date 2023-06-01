7191b296ae5223d0d16bd35a10b443af124448de
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-06-01 14:46:36 -0300

    Paginação, SEO, url canonico

--- Django/book-examples/example-1/mysite/blog/models.py
+++ Django/book-examples/example-1/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 # Post model that will allow us to store blog posts in the database
@@ -19,7 +20,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)  # Post title
-    slug = models.SlugField(max_length=250)  # Translate into a varchar column in the SQL
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')  # Translate into a varchar column in the SQL
     author = models.ForeignKey(User,         # Criando uma relação many-to-one
                                on_delete=models.CASCADE,  # Vai deletar tudo que estiver relacionado ao author
                                related_name='blog_posts')  # Especificar a relação reversa de User to Post
@@ -44,3 +46,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
