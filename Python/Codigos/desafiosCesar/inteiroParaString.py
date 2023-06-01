9630f9dc195dcbbb9ba03ce61d80db1b9807e1fd
Daniel Assuncao <assuncao.daniel89@gmail.com>
2023-06-01 18:25:21 -0300

    Pythonic: variaveis

--- Python/Codigos/desafiosCesar/inteiroParaString.py
+++ Python/Codigos/desafiosCesar/inteiroParaString.py
@@ -1,10 +1,10 @@
 def menu(opcao: int, controle: bool) -> bool:
-    if (opcao == 1):
+    if opcao == 1:
         print("Insira um valor de 0 a 200")
         numero = int(input())
         numeroParaInt(numero)
         return controle
-    elif (opcao == 2):
+    elif opcao == 2:
         print("Saindo do programa")
         controle = False
         return controle
@@ -13,167 +13,165 @@ def menu(opcao: int, controle: bool) -> bool:
         return controle
 
 
-def unidade(numerosList: list, indice):
-    if int(numerosList[indice]) == 0:
-        numerosList[indice] = "zero"
-        return numerosList
-    elif int(numerosList[indice]) == 1:
-        numerosList[indice] = "um"
-        return numerosList
-    elif int(numerosList[indice]) == 2:
-        numerosList[indice] = "dois"
-        return numerosList
-    elif int(numerosList[indice]) == 3:
-        numerosList[indice] = "tres"
-        return numerosList
-    elif int(numerosList[indice]) == 4:
-        numerosList[indice] = "quatro"
-        return numerosList
-    elif int(numerosList[indice]) == 5:
-        numerosList[indice] = "cinco"
-        return numerosList
-    elif int(numerosList[indice]) == 6:
-        numerosList[indice] = "seis"
-        return numerosList
-    elif int(numerosList[indice]) == 7:
-        numerosList[indice] = "sete"
-        return numerosList
-    elif int(numerosList[indice]) == 8:
-        numerosList[indice] = "oito"
-        return numerosList
-    elif int(numerosList[indice]) == 9:
-        numerosList[indice] = "nove"
-        return numerosList
+def unidade(numeros_list: list, indice):
+    if int(numeros_list[indice]) == 0:
+        numeros_list[indice] = "zero"
+        return numeros_list
+    elif int(numeros_list[indice]) == 1:
+        numeros_list[indice] = "um"
+        return numeros_list
+    elif int(numeros_list[indice]) == 2:
+        numeros_list[indice] = "dois"
+        return numeros_list
+    elif int(numeros_list[indice]) == 3:
+        numeros_list[indice] = "tres"
+        return numeros_list
+    elif int(numeros_list[indice]) == 4:
+        numeros_list[indice] = "quatro"
+        return numeros_list
+    elif int(numeros_list[indice]) == 5:
+        numeros_list[indice] = "cinco"
+        return numeros_list
+    elif int(numeros_list[indice]) == 6:
+        numeros_list[indice] = "seis"
+        return numeros_list
+    elif int(numeros_list[indice]) == 7:
+        numeros_list[indice] = "sete"
+        return numeros_list
+    elif int(numeros_list[indice]) == 8:
+        numeros_list[indice] = "oito"
+        return numeros_list
+    elif int(numeros_list[indice]) == 9:
+        numeros_list[indice] = "nove"
+        return numeros_list
     else:
         pass
 
 
-def dezena(numerosList: list, indice):
-    if int(numerosList[indice]) == 0:
-        numerosList[indice] = ""
-        return numerosList
-    if int(numerosList[indice]) == 1:
-        numerosList[indice] = "dez"
-        return numerosList
-    elif int(numerosList[indice]) == 2:
-        numerosList[indice] = "vinte"
-        return numerosList
-    elif int(numerosList[indice]) == 3:
-        numerosList[indice] = "trinta"
-        return numerosList
-    elif int(numerosList[indice]) == 4:
-        numerosList[indice] = "quarenta"
-        return numerosList
-    elif int(numerosList[indice]) == 5:
-        numerosList[indice] = "cinquenta"
-        return numerosList
-    elif int(numerosList[indice]) == 6:
-        numerosList[indice] = "sessenta"
-        return numerosList
-    elif int(numerosList[indice]) == 7:
-        numerosList[indice] = "setenta"
-        return numerosList
-    elif int(numerosList[indice]) == 8:
-        numerosList[indice] = "oitenta"
-        return numerosList
-    elif int(numerosList[indice]) == 9:
-        numerosList[indice] = "noventa"
-        return numerosList
+def dezena(numeros_list: list, indice):
+    if int(numeros_list[indice]) == 0:
+        numeros_list[indice] = ""
+        return numeros_list
+    if int(numeros_list[indice]) == 1:
+        numeros_list[indice] = "dez"
+        return numeros_list
+    elif int(numeros_list[indice]) == 2:
+        numeros_list[indice] = "vinte"
+        return numeros_list
+    elif int(numeros_list[indice]) == 3:
+        numeros_list[indice] = "trinta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 4:
+        numeros_list[indice] = "quarenta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 5:
+        numeros_list[indice] = "cinquenta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 6:
+        numeros_list[indice] = "sessenta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 7:
+        numeros_list[indice] = "setenta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 8:
+        numeros_list[indice] = "oitenta"
+        return numeros_list
+    elif int(numeros_list[indice]) == 9:
+        numeros_list[indice] = "noventa"
+        return numeros_list
     else:
         pass
 
 
-def dez(numerosList: list, indice):
-    if int(numerosList[indice]) == 0:
-        numerosList[indice] = "dez"
-        return numerosList[indice]
-    elif int(numerosList[indice]) == 1:
-        numerosList[indice] = "onze"
-        return numerosList[indice]
-    elif int(numerosList[indice]) == 2:
-        numerosList[indice] = "doze "
-        return numerosList[indice]
-    elif int(numerosList[indice]) == 3:
-        numerosList[indice] = "treze"
-        return numerosList
-    elif int(numerosList[indice]) == 4:
-        numerosList[indice] = "quatorze"
-        return numerosList
-    elif int(numerosList[indice]) == 5:
-        numerosList[indice] = "quinze"
-        return numerosList
-    elif int(numerosList[indice]) == 6:
-        numerosList[indice] = "dezesseis"
-        return numerosList
-    elif int(numerosList[indice]) == 7:
-        numerosList[indice] = "dezessete"
-        return numerosList
-    elif int(numerosList[indice]) == 8:
-        numerosList[indice] = "dezoito"
-        return numerosList
-    elif int(numerosList[indice]) == 9:
-        numerosList[indice] = "dezenove"
-        return numerosList
+def dez(numeros_list: list, indice):
+    if int(numeros_list[indice]) == 0:
+        numeros_list[indice] = "dez"
+        return numeros_list[indice]
+    elif int(numeros_list[indice]) == 1:
+        numeros_list[indice] = "onze"
+        return numeros_list[indice]
+    elif int(numeros_list[indice]) == 2:
+        numeros_list[indice] = "doze "
+        return numeros_list[indice]
+    elif int(numeros_list[indice]) == 3:
+        numeros_list[indice] = "treze"
+        return numeros_list
+    elif int(numeros_list[indice]) == 4:
+        numeros_list[indice] = "quatorze"
+        return numeros_list
+    elif int(numeros_list[indice]) == 5:
+        numeros_list[indice] = "quinze"
+        return numeros_list
+    elif int(numeros_list[indice]) == 6:
+        numeros_list[indice] = "dezesseis"
+        return numeros_list
+    elif int(numeros_list[indice]) == 7:
+        numeros_list[indice] = "dezessete"
+        return numeros_list
+    elif int(numeros_list[indice]) == 8:
+        numeros_list[indice] = "dezoito"
+        return numeros_list
+    elif int(numeros_list[indice]) == 9:
+        numeros_list[indice] = "dezenove"
+        return numeros_list
     else:
         pass
 
 
-def centena(numerosList: list, indice):
-    if int(numerosList[indice]) == 1:
-        if int(numerosList[1]) == 0 and int(numerosList[2]) == 0:
-            numerosList[indice] = "cem"
-            return numerosList
+def centena(numeros_list: list, indice):
+    if int(numeros_list[indice]) == 1:
+        if int(numeros_list[1]) == 0 and int(numeros_list[2]) == 0:
+            numeros_list[indice] = "cem"
+            return numeros_list
         else:
-            numerosList[indice] = "cento"
-            return numerosList
-    elif int(numerosList[indice]) == 2:
-        numerosList[indice] = "duzentos"
-        return numerosList
+            numeros_list[indice] = "cento"
+            return numeros_list
+    elif int(numeros_list[indice]) == 2:
+        numeros_list[indice] = "duzentos"
+        return numeros_list
     else:
         pass
 
 
 def numeroParaInt(numero: int):
-    if (numero <= 200 and numero >= 0):
-        numerosList = str(numero)  # TypeCasting para str
-        # print(numerosList)
-        numerosList = list(numerosList)  # TypeCasting para list
-        # print(numerosList)
-        # print(len(numerosList))
+    if 200 >= numero >= 0:
+        numeros_list = str(numero)  # TypeCasting para str
+        numeros_list = list(numeros_list)  # TypeCasting para list
 
-        if len(numerosList) == 1:  # Validar o tamanho da list
-            resultado = unidade(numerosList, indice=0)
+        if len(numeros_list) == 1:  # Validar o tamanho da list
+            resultado = unidade(numeros_list, indice=0)
             print(resultado)
 
-        elif len(numerosList) == 2:  # Validar o tamanho da list dezena
-            if int(numerosList[0]) == 1:  # Verificar se o numero começa com 1.
-                resultado = dez(numerosList, indice=1)  # Funcao especial para o caso dos numeros dez,onze,doze....
+        elif len(numeros_list) == 2:  # Validar o tamanho da list dezena
+            if int(numeros_list[0]) == 1:  # Verificar se o numero começa com 1.
+                resultado = dez(numeros_list, indice=1)  # Funcao especial para o caso dos numeros dez,onze,doze....
                 if type(resultado) == list:  # Solucao para numero >= doze; por algum motivo dava erro de Type
                     resultado.pop(0)
                 print(resultado)
-            elif int(numerosList[1]) == 0: # Valida se é um numero apenas q termina em 0: 20, 10, 40...
-                resultado = dezena(numerosList, indice=0)
+            elif int(numeros_list[1]) == 0:  # Valida se é um numero apenas q termina em 0: 20, 10, 40...
+                resultado = dezena(numeros_list, indice=0)
                 print(resultado[0])
             else:
-                resultado = dezena(numerosList, indice=0)
-                resultado += unidade(numerosList, indice=1)
+                resultado = dezena(numeros_list, indice=0)
+                resultado += unidade(numeros_list, indice=1)
                 print(resultado[0] + " e " + resultado[1])  # Concatenando
 
 
-        elif len(numerosList) == 3:   # Validar o tamanho da list centena
-            resultado = centena(numerosList, indice=0)
-            if int(numerosList[1]) == 1:  # Verificando se entra no "dez, onze..."
-                resultado += dez(numerosList, indice=2)
+        elif len(numeros_list) == 3:  # Validar o tamanho da list centena
+            resultado = centena(numeros_list, indice=0)
+            if int(numeros_list[1]) == 1:  # Verificando se entra no "dez, onze..."
+                resultado += dez(numeros_list, indice=2)
                 resultado.remove('1')
                 print(resultado[0] + " e " + resultado[1])
-            elif int(numerosList[1]) == 0 and int(numerosList[2]) == 0:   # Valida se é um numero "redondo" tipo 200,100...
+            elif int(numeros_list[1]) == 0 and int(
+                    numeros_list[2]) == 0:  # Valida se é um numero "redondo" tipo 200,100...
                 print(resultado[0])
-            elif int(numerosList[2]) == 0:   # Valida se é um numero apenas q termina em 0: 120, 110, 140...
-                resultado = dezena(numerosList, indice=1)
+            elif int(numeros_list[2]) == 0:  # Valida se é um numero apenas q termina em 0: 120, 110, 140...
+                resultado = dezena(numeros_list, indice=1)
                 print(resultado[0] + " e " + resultado[1])
             else:
-                resultado += dezena(numerosList, indice=1)
-                resultado += unidade(numerosList, indice=2)
+                resultado += dezena(numeros_list, indice=1)
+                resultado += unidade(numeros_list, indice=2)
                 print(resultado[0] + " e " + resultado[1] + " e " + resultado[2])
 
     else:
@@ -186,8 +184,7 @@ controle: bool = True
 while controle:
     print("""
     1- para entrar no programa
-    2- para sair da execucao
+    2- para sair da execução
     """)
     opcao = int(input())
-    print(opcao)
     controle = menu(opcao, controle)
