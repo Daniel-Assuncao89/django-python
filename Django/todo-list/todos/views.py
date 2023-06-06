29df727f3d5dc8782935ffe4a5b1de8c15232b1f
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-06-06 17:00:44 -0300

    CRUD finalizado: funcao salvar, editar, update e delete

--- Django/todo-list/todos/views.py
+++ Django/todo-list/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 # Create your views here.
 
@@ -13,3 +13,27 @@ def salvar(request):
     Todo.objects.create(task=task)
     tasks = Todo.objects.all()
     return render(request, "index.html", {"tasks": tasks})
+
+
+def editar(request, id):
+    task = Todo.objects.get(id=id)
+    status = Todo.Status.labels
+    return render(request, "update.html", {"task": task,
+                                           "status": status})
+
+
+def update(request, id):
+    task_html = request.POST.get("task")
+    status_html = request.POST.get("choices")
+    print(status_html)
+    task_obj = Todo.objects.get(id=id)
+    task_obj.task = task_html
+    task_obj.status = status_html
+    task_obj.save()
+    return redirect(home)
+
+
+def delete(request, id):
+    task_obj = Todo.objects.get(id=id)
+    task_obj.delete()
+    return redirect(home)
