c7785494a1f07845b5b9fad8d213306b0148e28e
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-06-15 13:07:22 -0300

    URL para nova view

--- Django/udemy/projeto1/recipes/urls.py
+++ Django/udemy/projeto1/recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
 
 
 urlpatterns = [
-    path('', home),
-
+    path('', views.home),
+    path('recipe/<int:id_recipe>/', views.recipe)
 ]
--- Django/udemy/projeto1/recipes/views.py
+++ Django/udemy/projeto1/recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Daniel Assuncao',
     })
+
+
+def recipe(request, id_recipe):
+    return render(request, 'recipes/pages/recipe.html', context={
+        'name': 'Daniel Assuncao',
+    })
