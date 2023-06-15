cf13a0bcab39a8adf790d5b104987593d4fa83c4
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-06-15 15:36:05 -0300

    Injetando dados na view da home

--- Django/udemy/projeto1/recipes/views.py
+++ Django/udemy/projeto1/recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Daniel Assuncao',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id_recipe):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Daniel Assuncao',
+        'recipe': make_recipe(),
     })
