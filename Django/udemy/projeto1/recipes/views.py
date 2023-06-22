04429fa60d2d4aed5b2d7d63aee35335c540391c
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-06-22 19:24:40 -0300

    Pagination e python-dotenv

--- Django/udemy/projeto1/recipes/views.py
+++ Django/udemy/projeto1/recipes/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Recipe
-# Create your views here.
+
+import os
+
+PER_PAGES = os.environ.get('PER_PAGE', 4)
 
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
+    page_obj = make_pagination(request, recipes, PER_PAGES)
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
+        'page_obj': page_obj,
     })
+
+
+def make_pagination(request, queryset, per_page):
+    paginator = Paginator(queryset, per_page)
+    page_number = request.GET.get('page', 1)
+    print(request.GET)
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 def category(request, id_category):
@@ -19,8 +35,11 @@ def category(request, id_category):
             is_published=True,
         ).order_by('-id'))
 
+    page_obj = make_pagination(request, recipes, PER_PAGES)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'page_obj': page_obj,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
