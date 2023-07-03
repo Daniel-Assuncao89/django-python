2d86ee221509e0a380141edb0dddc77b73089f12
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-07-03 17:20:44 -0300

    validação de dados via clean_field e clean

--- Django/udemy/projeto1/authors/forms.py
+++ Django/udemy/projeto1/authors/forms.py
@@ -1,8 +1,41 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be'
+            'at least 8 caracters.'
+        )
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +45,55 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password'
         ]
+
+        # exclude = [
+        #     'first_name'
+        # ]
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password'
+        }
+        help_texts = {
+            'email': 'The email must be valid'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This filed must not be empty',
+                'max_length': 'This field must have less then...',
+            }
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite "atenção" no campo password',
+                code='invalid'
+            )
+
+        return data
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise ValidationError({
+                'password': 'Password do not match',
+                'password2': 'Password do not match',
+            })
