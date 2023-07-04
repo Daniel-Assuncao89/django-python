ba679f746d68a63a762ef56bf0a46866e6d1e09a
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-07-04 16:59:57 -0300

    tornando email como unico para cada usuario

--- Django/udemy/projeto1/authors/forms.py
+++ Django/udemy/projeto1/authors/forms.py
@@ -102,6 +102,16 @@ class RegisterForm(forms.ModelForm):
 
         return data
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError(
+                'User e-mail already exist',
+                code='invalid'
+            )
+
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get('password')
