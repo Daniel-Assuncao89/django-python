bd252bfa5175695e24d458bf70feb3006c9c9534
Daniel-Assuncao89 <assuncao.daniel89@gmail.com>
2023-07-07 11:25:03 -0300

    Adição de campos para visualização, edição, link e ordenação no django admin

--- Django/udemy/projeto1/recipes/admin.py
+++ Django/udemy/projeto1/recipes/admin.py
@@ -11,7 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'author', 'title', 'created_at', 'is_published']
+    list_display_links = 'title', 'created_at',
+    search_fields = 'id', 'title', 'slug', 'description', 'preparation_steps',
+    list_filter = 'category', 'author', 'is_published', 'preparation_steps_is_html',
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = '-id',
 
 
 admin.site.register(Category, CategoryAdmin)
